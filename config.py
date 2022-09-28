847892658baf1657d7ec14e15673cd9cfbeec709
jackcokwanwaishing <114588882+jackcokwanwaishing@users.noreply.github.com>
2022-09-28 23:22:32 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cfi9rvp5miwn.us-east-1.rds.amazonaws.com"
+customhost = "employee.cvizqxos8p8v.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "jackcokwanwaishing-employee"
+custombucket = "lowchoonkeat-employee"
 customregion = "us-east-1"
