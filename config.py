d2776a86c38d620f449b7f8837b6805f5f81ec8b
jackcokwanwaishing <114588882+jackcokwanwaishing@users.noreply.github.com>
2022-09-29 11:14:51 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-customhost = "employee.cfi9rvp5miwn.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "jackcokwanwaishing-employee"
-customregion = "us-east-1"
